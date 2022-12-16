54c636d4d1af6e35f19fbbfdedc34b81fd65883c
Duckdayne123 <115419886+Duckdayne123@users.noreply.github.com>
2022-12-16 11:38:50 +0000

    Add files via upload

---
+++ ttmxh/public/js/config.js
@@ -0,0 +1,59 @@
+const __DATA__ = {
+    'config' : {
+        'title' : 'TRƯƠNG THANH ANH TUẤN',
+        'favicon' : 'at.jpg',
+        'notice': 'TRUONG THANH ANH TUAN 2008'
+    },
+    'avatar-image' : 'at.jpg',
+    'name' : 'TRƯƠNG THANH ANH TUẤN ', 
+    'is_tick' : true,
+    'skills' : [
+        'DDoS Attack ',
+        'Designer',
+    ],
+    'bio-storys' : [
+        {
+            'image' : 'at.jpg',
+            'content' : 'LIÊN HỆ ADMIN',
+            'author' : 'TRƯƠNG THANH ANH TUẤN',
+        },
+    ],
+    'links' : [
+        {
+            'image' : 'wallet.png',
+            'title' : 'Thông Tin Thanh Toán', 
+            'show-value' : 'TRƯƠNG THANH ANH TUẤN',
+            'value' : 'https://xbinzh.ml/PayMent/',
+            'type' : 0,
+        },
+        {
+            'image' : 'facebook.jpg',
+            'title' : 'Facebook', 
+            'show-value' : '@613907899' ,
+            'value' : 'https://www.facebook.com/profile.php?id=613907899',
+            'type' : 0,
+        },
+        {
+            'image' : 'zalo.jpg',
+            'title' : 'Zalo', 
+            'show-value' : 'Trương Thanh Anh Tuấn',
+            'value' : 'https://zalo.me/0338348428',
+            'type' : 0,
+        },
+        {
+            'image' : 'phone.jpg',
+            'title' : 'Hotline', 
+            'show-value' : '0338.348.428',
+            'value' : 'tel:0338.348.428',
+            'type' : 0,
+        },
+        {
+            'image' : 'gmail.jpg',
+            'title' : 'Gmail', 
+            'show-value' : 'truongthanhanhtuandn40',
+            'value' : 'mailto:truongthanhanhtuandn40',
+            'type' : 0,
+        },
+    ]
+};
+
